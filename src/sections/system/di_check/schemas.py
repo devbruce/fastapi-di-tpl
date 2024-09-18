4d9045bf51bbd342d8890cec699942770c280b52
devbruce <bruce93k@gmail.com>
2024-09-19 01:19:53 +0900

    docs(di-check): add examples of DependencyInjectorCheckResponse

--- src/sections/system/di_check/schemas.py
+++ src/sections/system/di_check/schemas.py
@@ -1,5 +1,20 @@
+from typing import cast
+
 from pydantic import BaseModel
+from pydantic import ConfigDict
+from pydantic.config import JsonDict
+
+from sections.system.di_check.examples import response_examples
 
 
 class DependencyInjectorCheckResponse(BaseModel):
     injected_config_value: str
+
+    model_config = ConfigDict(
+        json_schema_extra=cast(
+            JsonDict,
+            {
+                "examples": response_examples,
+            },
+        )
+    )
